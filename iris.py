5418cec21c1c7240593c23e9e9373265122937a3
Ben Lee <bendelonlee@gmail.com>
2019-04-23 12:05:54 -0600

    Splits up data

--- iris.py
+++ iris.py
@@ -15,3 +15,14 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
+
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+import pdb; pdb.set_trace()
